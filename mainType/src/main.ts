4148ec38de610f5891e699c09fd8d3d876775daf
khamzaevasad <xamzayevasad442@gmail.com>
2025-10-08 01:16:28 +0900

    main type

---
+++ mainType/src/main.ts
@@ -0,0 +1,17 @@
+// asosiy typelar boolen, number, string
+
+// string
+let name: string = "Leo";
+// console.log(name);
+// console.log(typeof name);
+
+// number
+const salary: number = 2000;
+const bonus = "500";
+const totalSalary = salary + bonus;
+// console.log(`${totalSalary} ${typeof totalSalary}`);
+
+// boolen
+const isMarried: boolean = false;
+// console.log(isMarried);
+// console.log(typeof isMarried);
